1d533a6cb86e4b1069af58cce0b2c4f296c32cf2
Lane Bryer <lanebryer@gmail.com>
2018-02-07 17:14:58 -0500

    Final

--- tcpclient.c
+++ tcpclient.c
@@ -12,39 +12,53 @@
 
 #define buffer 1024
 
+ char userName[15];
+ char serverName[15];
 
-void getUserName(char* userName)
+//Simply retrieves the user name for the client from the user
+void getUserName()
 {
     do
     {
         printf("Enter a user name of up to 10 characters: ");
-        scanf("%s", userName);
+        fgets(userName, 15, stdin);
     } while(strlen(userName) > 10);
+	userName[strcspn(userName, "\n")] = 0;
 }
 
-void exchangeUserNames(char* userName, char* serverName, int sockFD)
+//This sends the client's user name to the server and then waits to get the server's user name back
+void exchangeUserNames(int sockFD)
 {
-    int result;
+    int result;	
     result = send(sockFD, userName, strlen(userName), 0);
     result = recv(sockFD, serverName, buffer, 0);
+	if(result < 0)
+	{
+		printf("ERROR\n");
+	}
 }
 
-void sendMsg(int sockFD, char* userName)
+//This sends a message to the server.  It gets the client's message input and strips the newline and then sends.
+//It checks for an input of "\quit" and exits if found.
+void sendMsg(int sockFD)
 {
     int result;
     char outbound[buffer];
     memset(outbound, 0, sizeof(outbound));
     printf("%s> ", userName);
     fgets(outbound, buffer, stdin);
+	outbound[strcspn(outbound, "\n")] = 0;
     result = send(sockFD, outbound, buffer, 0);
-    if(strcmp(outbound, "\\quit\n") == 0)
+    if(strcmp(outbound, "\\quit") == 0)
     {
-        printf("Exiting client...");
+        printf("Exiting client...\n");
         exit(0);
     }
 }
 
-void recvMsg(int sockFD, char* serverName)
+//This receives a message from the server.  If the message is "\quit", the program exits".  Otherwise, it prints
+//the received message.
+void recvMsg(int sockFD)
 {
     int result;
     char inbound[buffer];
@@ -56,18 +70,20 @@ void recvMsg(int sockFD, char* serverName)
         exit(0);
     }
     
-    printf("%s> %s", serverName, inbound);
+    printf("%s> %s\n", serverName, inbound);
 }
 
-void chat(int sockFD, char* userName, char* serverName)
+//This simply loops endlessly, sending and then receiving messages, until "\quit" is entered
+void chat(int sockFD)
 {
     while(1)
     {
-        sendMsg(sockFD, userName);
-        recvMsg(sockFD, serverName);
+        sendMsg(sockFD);
+        recvMsg(sockFD);
     }
 }
 
+//The main routine establishes the connection, gets a user name, exchanges user names with the server, and then begins the chat loop.
 int main(int argc, char *argv[])
 {
     if(argc != 3)
@@ -79,8 +95,6 @@ int main(int argc, char *argv[])
     int sockFD, portNumber, result;
     struct addrinfo hints;
     struct addrinfo* res = 0;
-    char userName[15];
-    char serverName[15];
     memset(userName, 0, sizeof(userName));
     memset(serverName, 0, sizeof(serverName));
     memset(&hints, 0, sizeof(hints));
@@ -95,11 +109,21 @@ int main(int argc, char *argv[])
     }
     
     sockFD = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockFD == -1)
+	{
+		perror("Socket error!\n");
+	}
+	
     result = connect(sockFD, res->ai_addr, res->ai_addrlen);
+	if (result == -1)
+	{
+		perror("Error connecting!\n");
+	}
+	
     freeaddrinfo(res);
     getUserName(userName);
-    exchangeUserNames(userName, serverName, sockFD);
-    chat(sockFD, userName, serverName);
+    exchangeUserNames(sockFD);
+	chat(sockFD);
     
     return 0;
 }
--- tcpserver.py
+++ tcpserver.py
@@ -3,6 +3,7 @@
 from socket import *
 import sys
 
+#this block checks for the correct number of arguments
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print 'Please enter a port number to listen on'
@@ -14,29 +15,46 @@ serverName = "server"
 
 buffer = 1024
 
+#These three lines establish the socket, bind it to the port given by the user, and then listens on that port
 s = socket(AF_INET, SOCK_STREAM)
 s.bind(('', int(port)))
 s.listen(1)
 
+#this exchanges user names with the client
 def exchangeNames(conn):
     userName = conn.recv(buffer)
     userName = userName + "> "
-    conn.send(serverName)
+    conn.sendall(serverName)
     return userName
-    
+   
+#this is the basic chat loop which receives and then sends a message in a loop   
 def chat(conn):
     while(1):
-        recvMsg(conn)
-        sendMsg(conn)
-    
+        quit = recvMsg(conn)        
+        if (quit == 1):
+            print "Client closed connection..."
+            break
+        outboundMsg = sendMsg(conn)
+        if (outboundMsg == "\quit"):
+            print "Closing client...\n"
+            break
+   
+#this sends a message to the client by getting user input and then sending   
 def sendMsg(conn):
-    outbound = raw_input("{}> ".format(serverName))
-    print outbound
+    outboundMsg = raw_input("server> ")
+    conn.sendall(outboundMsg)
+    return outboundMsg
 
+#this receives a message from the client
 def recvMsg(conn):
-    inbound = conn.recv(buffer)
-    print inbound
-    
+    inboundMsg = conn.recv(buffer)
+    if ("\quit" in inboundMsg):
+	    return 1
+    else:		
+        print "{}{}".format(userName, inboundMsg)
+        return 0
+ 
+#this loop exists so that when a connection is closed iwth a client, it begins to listen for a new connection 
 while(1):
     conn, addr = s.accept()
     userName = exchangeNames(conn)
